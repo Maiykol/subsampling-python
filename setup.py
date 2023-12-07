e7351c9d38bef90a4a6398f557e8389fce1303ff
Hartung, Michael <michael.hartung@uni-hamburg.de>
2023-12-07 12:43:58 +0100

    version 0.1.19

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dazer',
-    version='0.1.18',
+    version='0.1.19',
     author='Maiykol',
     author_email='michael.hartung@uni-hamburg.de',
     packages=['dazer'],
